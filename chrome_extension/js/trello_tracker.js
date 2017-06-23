6388ed68167068bc40f059a6e513e3884e4b31a4
ptelevich <pavel.televic@teqniksoft.com>
2017-06-23 20:10:27 +0300

    added constant for trello url

--- chrome_extension/js/trello_tracker.js
+++ chrome_extension/js/trello_tracker.js
@@ -1,6 +1,7 @@
 var TrelloTracker = function()
 {
     const APP_ROOTS_URL = 'https://trello.personal.loc';
+    const TRELLO_URL = 'https://trello.com/1';
     var public = this;
     var private = {
         appConf: {
@@ -16,6 +17,11 @@ var TrelloTracker = function()
     public.appRootUrl = function()
     {
         return APP_ROOTS_URL;
+    };
+
+    public.trelloUrl = function()
+    {
+        return TRELLO_URL;
     };
 
     public.inStart = function(content)
@@ -34,7 +40,7 @@ var TrelloTracker = function()
             params += '&response_type='+private.appConf.response_type;
             params += '&expiration='+private.appConf.expiration;
             params += '&return_url='+private.appConf.return_url;
-            window.open("https://trello.com/1/connect?"+params, "authTrelloWindow", private.appConf.windowSize);
+            var trello_connect = window.open(TRELLO_URL + "/connect?"+params, "authTrelloWindow", private.appConf.windowSize);
         });
     };
 
@@ -65,34 +71,33 @@ var getHTML = function ( url, callback ) {
 
 var sendEstimation = function(estimation) {
     var trackerClass = new TrelloTracker();
-        var findCardId = window.location.href.match(/(trello\.com\/c\/)(.+?)(\/)/);
-        if (findCardId[2]) {
-            console.log(findCardId[2]);
-            getHTML('https://trello.com/1/cards/'+findCardId[2], function (data) {
-                function encodeData(data) {
-                    return Object.keys(data).map(function(key) {
-                        return [key, data[key]].map(encodeURIComponent).join("=");
-                    }).join("&");
-                }
-                var data = {
-                    b: data.idBoard,
-                    l: data.idList,
-                    c: data.id,
-                    t: estimation,
-                };
-                var querystring = encodeData(data);
-                console.log(querystring);
-                getHTML(trackerClass.appRootUrl() + '/tracker/save-time-trello/?'+encodeData(data), function (data) {
-                });
-            });
-        }
+    var findCardId = window.location.href.match(/(trello\.com\/c\/)(.+?)(\/)/);
+    if (findCardId[2]) {
+        console.log(findCardId[2]);
+        getHTML(trackerClass.trelloUrl() + '/cards/'+findCardId[2], function (data) {
+            function encodeData(data) {
+                return Object.keys(data).map(function(key) {
+                    return [key, data[key]].map(encodeURIComponent).join("=");
+                }).join("&");
+            }
+            var data = {
+                b: data.idBoard,
+                l: data.idList,
+                c: data.id,
+                t: estimation,
+            };
+            var querystring = encodeData(data);
+            console.log(querystring);
+            getHTML(trackerClass.appRootUrl() + '/tracker/save-time-trello/?'+encodeData(data), function (data) {});
+        });
+    }
 };
 
 $(function(){
     var trackerClass = new TrelloTracker();
     var def1 = $.Deferred();
 
-    getHTML('https://trello.com/1/members/me', function (data) {
+    getHTML(trackerClass.trelloUrl() + '/members/me', function (data) {
         var userId = data.id;
         def1.resolve(userId);
     });
